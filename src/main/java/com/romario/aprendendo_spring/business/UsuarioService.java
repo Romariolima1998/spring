3ad147098db2b67ccc5be53232c8a5d7aee5591e
Romario lima <romariolima1006@gmail.com>
2025-09-01 14:39:48 -0300

    first commit

---
+++ src/main/java/com/romario/aprendendo_spring/business/UsuarioService.java
@@ -0,0 +1,43 @@
+package com.romario.aprendendo_spring.business;
+
+import com.romario.aprendendo_spring.infrastructure.entity.Usuario;
+import com.romario.aprendendo_spring.infrastructure.exceptions.ConflictException;
+import com.romario.aprendendo_spring.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Usuario salvarUsuario(Usuario usuario){
+        try{
+            emailExiste(usuario.getEmail());
+            usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+            return usuarioRepository.save(usuario);
+        }catch (ConflictException e){
+            throw new ConflictException("email ja cadastrado", e.getCause());
+        }
+
+    }
+
+    public void emailExiste(String email){
+        try{
+            boolean existe = verificaEmailExistente(email);
+            if(existe){
+                throw new ConflictException("email ja cadastrado");
+            }
+        } catch (ConflictException e) {
+            throw new ConflictException("email ja cadastrado ", e.getCause());
+        }
+    }
+
+    public boolean verificaEmailExistente(String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+
+}
---
+++ src/main/java/com/romario/aprendendo_spring/controller/UsuarioController.java
@@ -0,0 +1,38 @@
+package com.romario.aprendendo_spring.controller;
+
+import com.romario.aprendendo_spring.business.UsuarioService;
+import com.romario.aprendendo_spring.controller.dtos.UsuarioDTO;
+import com.romario.aprendendo_spring.infrastructure.entity.Usuario;
+import com.romario.aprendendo_spring.infrastructure.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping
+    public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.ok(usuarioService.salvarUsuario(usuario));
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(), usuarioDTO.getSenha())
+        );
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+    }
+}
